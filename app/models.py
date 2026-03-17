9057e64b88a0d57d61d58375eb6a2d5dde593cd3
traorearthurg <traore.arthurg@gmail.com>
2026-03-17 02:41:44 +0100

    Modif : valideur de models car Trace avec avec altitude en dessous du nv de la mer

--- app/models.py
+++ app/models.py
@@ -69,8 +69,8 @@ class TrailSchema(BaseModel):
     elevation_gain: Optional[float] = Field(None, example=500.0, ge=0)
     elevation_loss: Optional[float] = Field(None, example=500.0, ge=0)
     start_location: Optional[str] = Field(None, example="45.0, 5.0")
-    altitude_max: Optional[float] = Field(None, example=2000.0, ge=0)
-    altitude_min: Optional[float] = Field(None, example=1000.0, ge=0)
+    altitude_max: Optional[float] = Field(None, example=2000.0)
+    altitude_min: Optional[float] = Field(None, example=1000.0)
     created_at: Optional[datetime.datetime] = Field(None, example="2023-01-01T12:00:00Z")
 
     points: List[TrailPointSchema] = []
@@ -79,7 +79,7 @@ class TrailSchema(BaseModel):
         "from_attributes": True,
     }
 
-    @field_validator("length", "elevation_gain", "elevation_loss", "altitude_max", "altitude_min")
+    @field_validator("length", "elevation_gain", "elevation_loss")
     def check_positive_values(cls, value: Optional[float]) -> Optional[float]:
         if value is not None and value < 0:
             raise ValueError("La valeur doit être positive")
