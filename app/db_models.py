bd60ae2c03498454e3bffa98a8f1be7a2485caeb
Timothee7 <timothee.jamin@protonmail.com>
2026-03-23 22:05:23 +0100

    Update db_models.py

--- app/db_models.py
+++ app/db_models.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String, Float, DateTime, Text, UniqueCon
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
-from app.database import Base
+from app.db_connection import Base
 
 
 # --- SQLAlchemy Models ---
